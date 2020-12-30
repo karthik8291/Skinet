a70dafcd02ca792fb83eedf1ade781f197147ad0
karthik8291 <lingam.karthikeyan@gmail.com>
2020-12-30 20:16:05 +0530

    End of session 6

--- Core/Interfaces/IGenericRepository.cs
+++ Core/Interfaces/IGenericRepository.cs
@@ -11,7 +11,7 @@ namespace Core.Interfaces
         Task<IReadOnlyList<T>> ListAllAsync();
         Task<T> GetEntityWithSpec(ISpecifications<T> spec);
         Task<IReadOnlyList<T>> ListAsync(ISpecifications<T> spec);
-        //  Task<int> CountAsync(ISpecification<T> spec);
+         Task<int> CountAsync(ISpecifications<T> spec);
         //  void Add(T entity);
         //  void Update(T entity);
         //  void Delete(T entity);
--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,6 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using Core.Entities;
 using System.Reflection;
+using System.Linq;
+using System;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace  Infrastructure.Data
 {
@@ -19,6 +22,26 @@ namespace  Infrastructure.Data
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            if (Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite")
+            {
+                foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+                {
+                    var properties = entityType.ClrType.GetProperties().Where(p => p.PropertyType == typeof(decimal));
+                    var dateTimeProperties = entityType.ClrType.GetProperties()
+                        .Where(p => p.PropertyType == typeof(DateTimeOffset));
+
+                    foreach (var property in properties)
+                    {
+                        modelBuilder.Entity(entityType.Name).Property(property.Name).HasConversion<double>();
+                    }
+
+                    foreach (var property in dateTimeProperties)
+                    {
+                        modelBuilder.Entity(entityType.Name).Property(property.Name)
+                            .HasConversion(new DateTimeOffsetToBinaryConverter());
+                    }
+                }
+            }
         }
         
     }
